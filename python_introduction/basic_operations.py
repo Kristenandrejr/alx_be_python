77cc2d70f7df0da8f1f1afccbf7442da180232fd
Kristenandrejr <kristenkrisjr@gmail.com>
2024-09-13 14:02:28 +0100

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+# Define two numbers
+number1 = 10  # Correctly assigned value for number1
+number2 = 5   # Correctly assigned value for number2
+# Perform basic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+# Print the results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
