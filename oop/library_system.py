72ab8b27c213b65b6a16395b08c5ca9323bc55d8
Christian Edeh <kristenkrisjr@gmail.com>
2024-10-12 12:03:26 +0100

    Add library system with inheritance and composition example

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,5 +1,6 @@
 # library_system.py
 
+# Base class for books
 class Book:
     def __init__(self, title, author):
         self.title = title
@@ -8,6 +9,7 @@ class Book:
     def get_info(self):
         return f"Book: {self.title} by {self.author}"
 
+# Subclass for eBooks
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
@@ -16,6 +18,7 @@ class EBook(Book):
     def get_info(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
+# Subclass for printed books
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
@@ -24,6 +27,7 @@ class PrintBook(Book):
     def get_info(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
+# Library class to hold a collection of books
 class Library:
     def __init__(self):
         self.books = []
@@ -32,5 +36,8 @@ class Library:
         self.books.append(book)
 
     def list_books(self):
-        for book in self.books:
-            print(book.get_info())
+        if not self.books:
+            print("The library is empty!")
+        else:
+            for book in self.books:
+                print(book.get_info())
